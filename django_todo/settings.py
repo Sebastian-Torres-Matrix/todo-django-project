bd0bdc68356bcecb400a0a9dded5f2caf0ed65ef
Sebastian Torres <sebastian_torres@hotmail.se>
2020-08-26 18:47:01 +0000

    Set up environment variables & automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +88,6 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 
 # Password validation
